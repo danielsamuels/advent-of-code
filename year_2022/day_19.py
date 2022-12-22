a22b5cbe690f3635bbf04d92b7b4c96b687e6ad5
Daniel Finch <daniel.samuels1@gmail.com>
2022-12-22 08:49:29 +0000

    Continue work on day 19

--- year_2022/day_19.py
+++ year_2022/day_19.py
@@ -1,14 +1,13 @@
-import os
-import random
+import copy
+import math
+import operator
 import re
-from multiprocessing import Pool
 from typing import Union
-
-from tqdm import trange
 
 Material = Union['geode', 'obsidian', 'clay', 'ore']
 materials: list[Material] = ['geode', 'obsidian', 'clay', 'ore']
-
+time_limit = 24
+inf = float('inf')
 
 class Blueprint:
     robot_costs: dict[Material, dict[Material, int]]
@@ -17,6 +16,7 @@ class Blueprint:
 
     def __init__(self, definition: str):
         results = list(map(int, re.findall(r'(\d+)', definition)))
+        self.identifier = results[0]
         self.robot_costs = {
             'ore': {'ore': results[1]},
             'clay': {'ore': results[2]},
@@ -30,10 +30,123 @@ class Blueprint:
             'geode': {'own': 0, 'pending': 0},
         }
         self.material_inventory = {'ore': 0, 'clay': 0, 'obsidian': 0, 'geode': 0}
+        assert all(v == 0 for v in self.material_inventory.values())
 
-    def can_afford(self, material: Material) -> bool:
+    def calculate_score(self):
+        print(f'\n== Blueprint {self.identifier} ==')
+        assert all(v == 0 for v in self.material_inventory.values())
+
+        for minute in range(1, time_limit + 1):
+            """
+            Order of operations:
+            - Purchase new robots
+            - Collect ores
+            - Activate purchased robots
+            """
+            print(f'\n== Minute {minute} ==')
+            self.maybe_make_purchase(minute)
+            self.collect_materials()
+            self.finish_building_robots()
+
+        return self.material_inventory['geode']
+
+    def purchase_robot(self, robot_type: Material, dry_run=False):
+        if not self.can_afford(robot_type):
+            raise ValueError('Unable to afford %s robot', robot_type)
+
+        robot_inventory = copy.deepcopy(self.robot_inventory)
+        material_inventory = copy.deepcopy(self.material_inventory)
+
+        if not dry_run:
+            expenditure = ' and '.join([
+                f'{amount} {material}'
+                for material, amount in self.robot_costs[robot_type].items()
+            ])
+            print(f'Spend {expenditure} to start building a {robot_type} robot.')
+
+        robot_inventory[robot_type]['pending'] += 1
+        for material, amount in self.robot_costs[robot_type].items():
+            material_inventory[material] -= amount
+
+        if not dry_run:
+            self.robot_inventory = robot_inventory
+            self.material_inventory = material_inventory
+
+        return robot_inventory, material_inventory
+
+    def maybe_make_purchase(self, minute: int):
+        # If we're near the end of the cycle, don't attempt to purchase anything
+        if minute == time_limit:
+            return
+
+        # If we can afford a geode-cracking robot, buy one immediately.
+        if self.can_afford('geode'):
+            return self.purchase_robot('geode')
+
+        geode_distance = self.distance_to('geode')
+        obsidian_distance = self.distance_to('obsidian')
+        if self.can_afford('obsidian'):
+            #  and geode_distance > 1
+            # Will purchasing one of these reduce the geode distance?
+            obsidian_ri, obsidian_mi = self.purchase_robot('obsidian', dry_run=True)
+            obsidian_gd = self.distance_to('geode', obsidian_ri, obsidian_mi)
+
+            if geode_distance == inf:
+                return self.purchase_robot('obsidian')
+
+            if minute + geode_distance > time_limit - 1 and minute + obsidian_gd > time_limit - 1:
+                return
+
+            if obsidian_gd <= geode_distance:
+                return self.purchase_robot('obsidian')
+
+        # Will we be able to afford a geode or obsidian robot *next* time
+        # _and_ have enough time for it to make a difference?
+
+        if minute < time_limit - 1 and geode_distance <= 1 or obsidian_distance <= 1:
+            return
+
+        # If purchasing a clay or ore robot reduces the time it takes to get
+        # an obsidian or geode robot, then do it. If it makes no difference
+        # then do it anyway.
+        if self.can_afford('clay'):
+            clay_ri, clay_mi = self.purchase_robot('clay', dry_run=True)
+            clay_gd = self.distance_to('geode', clay_ri, clay_mi)
+            clay_od = self.distance_to('obsidian', clay_ri, clay_mi)
+            if clay_gd == inf and clay_od == inf:
+                return self.purchase_robot('clay')
+
+            if (clay_gd != inf and clay_gd <= geode_distance) or (clay_od != inf and clay_od <= obsidian_distance):
+                return self.purchase_robot('clay')
+
+        # Finally, look at ore robot purchasing.
+        if self.can_afford('ore'):
+            # If we're already producing more ore than we're able to spend per
+            # minute, there's no point in purchasing more ore producing robots.
+            if self.robot_inventory['clay']['own'] >= max(robot.get('clay', 0) for robot in self.robot_costs.values()):
+                return
+
+            # Now return to the same question that was asked about clay earlier:
+            # Does purchasing an ore robot affect the distance to purchasing an obs/geode robot?
+            ore_ri, ore_mi = self.purchase_robot('ore', dry_run=True)
+            ore_gd = self.distance_to('geode', ore_ri, ore_mi)
+            ore_od = self.distance_to('obsidian', ore_ri, ore_mi)
+            if ore_gd == inf and ore_od == inf:
+                return self.purchase_robot('ore')
+
+            if (ore_gd != inf and ore_gd <= geode_distance) or (ore_od != inf and ore_od <= obsidian_distance):
+                return self.purchase_robot('ore')
+
+        # If we have more of this material than we're able to consume in the remaining time, don't build
+        # if materials[material] > max_material_consumption[material] * (24 - minute):
+        #     break
+
+    def can_afford(self, material: Material, inventory: dict[Material, int] = None) -> bool:
+        if inventory is None:
+            inventory = self.material_inventory
+
         return all([
-            self.material_inventory[material] >= amount
+            inventory[material] >= amount
             for material, amount in self.robot_costs[material].items()
         ])
 
@@ -45,8 +158,47 @@ class Blueprint:
             if self.can_afford(material)
         ]
 
-    def distance_to(self, material: Material):
-        print()
+    def distance_to(self,
+                    robot_type: Material,
+                    robot_inventory: dict[Material, dict[str, int]] = None,
+                    material_inventory: dict[Material, int] = None) -> float | int | None:
+        costs = self.robot_costs[robot_type]
+
+        if robot_inventory is None:
+            robot_inventory = self.robot_inventory
+
+        if material_inventory is None:
+            material_inventory = self.material_inventory
+
+        # Do we have at least one of each robot type needed to generate the required resources?
+        if not all(robot_inventory[material]['own'] > 0 for material in costs.keys()):
+            return inf
+
+        return max([
+            math.ceil((costs[material] - material_inventory[material]) / robot_inventory[material]['own'])
+            for material in costs.keys()
+        ])
+
+    def collect_materials(self):
+        for material, values in self.robot_inventory.items():
+            if count := values['own']:
+                self.material_inventory[material] += count
+                print("%(c)s %(m)s robot(s) collects %(c)s %(m)s; you now have %(t)s." % {
+                    'c': count,
+                    'm': material,
+                    't': self.material_inventory[material],
+                })
+
+    def finish_building_robots(self):
+        for material, values in self.robot_inventory.items():
+            if values['pending'] > 0:
+                self.robot_inventory[material]['own'] += values['pending']
+                self.robot_inventory[material]['pending'] = 0
+                own = self.robot_inventory[material]['own']
+                print(f'The new {material} robot is ready; you now have {own} of them.')
+
+        # We should have no more pending robots
+        assert not [r for r, v in self.robot_inventory.items() if v['pending'] > 0]
 
 
 class Day:
@@ -64,156 +216,16 @@ class Day:
             for need_key, need_value in need.items()
         ])
 
-    def would_afford_better(self, blueprint, robots, materials, robot_index):
-        next_round = {
-            material: materials[material] + robots[material]['built']
-            for material in self.materials
-        }
-        for robot_type in self.materials[:robot_index]:
-            robot = robots[robot_type]
-            if self.can_afford(blueprint[robot_type], next_round):
-                return True
-
-        return False
-
-    def will_afford_geode_or_obsidian_soon(self, blueprint, robots, materials, limit):
-        for in_round in range(1, limit):
-            next_round = {
-                material: materials[material] + (robots[material]['built'] * in_round)
-                for material in self.materials
-            }
-            if any([
-                self.can_afford(blueprint['geode'], next_round),
-                self.can_afford(blueprint['obsidian'], next_round),
-            ]):
-                return True
-
-        return False
-
-    def calculate_blueprint_scores(self, blueprint) -> int:
-        cheese = True
-
-        materials = {'ore': 0, 'clay': 0, 'obsidian': 0, 'geode': 0}
-        robots = {
-            'ore': {'built': 1, 'pending': 0},
-            'clay': {'built': 0, 'pending': 0},
-            'obsidian': {'built': 0, 'pending': 0},
-            'geode': {'built': 0, 'pending': 0},
-        }
-        max_material_consumption = {
-            material: max(robot.get(material, 0) for robot in blueprint.values())
-            for material in self.materials
-        }
-        time_limit = 24
-
-        for minute in range(1, time_limit + 1):
-            """
-            Order of operations:
-            - Purchase new robots
-            - Collect ores
-            - Activate purchased robots
-            """
-            print(f'== Minute {minute} ==')
-
-            # Given the current means of production, how far are we from
-            # being able to purchase an obsidian or geode robot?
-            distance_to_obsidian = 0
-            distance_to_geode = 0
-            # Does making a purchase of an ore or clay robot increase the distance to obs/geo?
-            # If no, then it's a "free" purchase, so do it.
-
-            # Never buy more clay robots than the most expensive robot costs in clay.
-
-            # If we have time to purchase something and have it make a difference
-            if minute < time_limit:
-                # Can we afford a geode-cracker?
-                if self.can_afford(blueprint['geode'], materials):
-                    expenditure = ' and '.join([
-                        f'{amount} {material}'
-                        for material, amount in blueprint['geode'].items()
-                    ])
-                    print(f'Spend {expenditure} to start building a geode robot.')
-
-                    robots['geode']['pending'] += 1
-                    for material, amount in blueprint['geode'].items():
-                        materials[material] -= amount
-
-                elif self.can_afford(blueprint['obsidian'], materials):
-                    if not self.would_afford_better(blueprint, robots, materials, 1):
-                        expenditure = ' and '.join([
-                            f'{amount} {material}'
-                            for material, amount in blueprint['obsidian'].items()
-                        ])
-                        print(f'Spend {expenditure} to start building a obsidian robot.')
-
-                        robots['obsidian']['pending'] += 1
-                        for material, amount in blueprint['obsidian'].items():
-                            materials[material] -= amount
-
-                else:
-                    for material in ['clay', 'ore']:
-                        # If we're producing more ore per minute than we can consume, don't build more ore robots
-                        if material == 'ore' and robots['ore']['built'] > max_material_consumption['ore']:
-                            break
-
-                        # If we have more of this material than we're able to consume in the remaining time, don't build
-                        if materials[material] > max_material_consumption[material] * (24 - minute):
-                            break
-
-                        # Can we afford to build a robot of this type?
-                        # Do we _want_ to build one?
-                        can_afford = self.can_afford(blueprint[material], materials)
-
-                        limit = min(3, 24 - minute)
-                        will_afford_better = self.will_afford_geode_or_obsidian_soon(blueprint, robots, materials,
-                                                                                     limit)
-                        if can_afford and not will_afford_better:
-                            expenditure = ' and '.join([
-                                f'{amount} {material}'
-                                for material, amount in blueprint[material].items()
-                            ])
-                            print(f'Spend {expenditure} to start building a {material} robot.')
-                            robots[material]['pending'] += 1
-                            materials['ore'] -= blueprint[material]['ore']
-                            break
-
-            # Run collection
-            for material, values in robots.items():
-                if count := robots[material]['built']:
-                    materials[material] += count
-                    print(
-                        f"{count} {material} robot(s) collects {count} {material}; you now have {materials[material]} {material}.")
-
-            # Turn pending robots into real ones
-            for material, values in robots.items():
-                if values['pending'] > 0:
-                    robots[material]['built'] += values['pending']
-                    robots[material]['pending'] = 0
-                    built = robots[material]['built']
-                    print(f'The new {material} robot is ready; you now have {built} of them.')
-
-            print()
-
-        return materials['geode']
-
-    def get_max_score(self, blueprint) -> int:
-        max_score = 0
-        for _ in trange(250_000):
-            score = self.calculate_blueprint_scores(blueprint)
-            max_score = max(max_score, score)
-
-        return max_score
-
     def run_step_1(self) -> int:
-        # scores = {i: 0 for i in range(len(self.blueprints))}
-        scores = map(self.calculate_blueprint_scores, self.blueprints)
+        scores = list(map(operator.methodcaller('calculate_score'), self.blueprints))
 
         # with Pool(min(os.cpu_count(), len(self.blueprints))) as p:
         #     scores = p.map(self.calculate_blueprint_scores, self.blueprints)
 
-        print(list(scores))
+        # Should be [9, 12]
+        print(scores)
         return sum([
-            k + 1 * v
+            (k + 1) * v
             for k, v in enumerate(scores)
         ])
 
