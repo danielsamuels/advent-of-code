58cc5a1c019b162b17c7920a2055fa530406cc23
Daniel Finch <dan.finch@hp.com>
2023-12-01 10:40:36 +0000

    Add template for 2023

---
+++ year_2023/_template/test_solution.py
@@ -0,0 +1,13 @@
+from solution import Day
+
+test_data = """
+
+""".strip()
+
+
+def test_run_step_1():
+    assert Day(test_data).run_step_1() == 0
+
+
+def test_run_step_2():
+    assert Day(test_data).run_step_2() == 0
