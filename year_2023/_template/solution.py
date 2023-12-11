7bad11862ae7d9d1c8e641035c5b57e42f9a606e
Daniel Finch <dan.finch@hp.com>
2023-12-11 10:35:13 +0000

    Move `get_data` to util function

--- year_2023/_template/solution.py
+++ year_2023/_template/solution.py
@@ -1,3 +1,6 @@
+from utils.data import get_data
+
+
 class Day:
     def __init__(self, data: str):
         self.data = data.splitlines()
@@ -10,13 +13,7 @@ class Day:
 
 
 if __name__ == "__main__":
-    from aocd import get_data
-    import pathlib
-
-    parts = pathlib.Path().absolute().parts
-    day = int(next(p[4:] for p in parts if p.startswith('day_')))
-    year = int(next(p[5:] for p in parts if p.startswith('year_')))
-    data = get_data(year=year, day=day)
+    data = get_data(__file__)
 
     day = Day(data)
     print(f'Step 1: {day.run_step_1()}')
