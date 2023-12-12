f29040874e680fe9b0f0ca9ea0e6857386634111
Daniel Finch <dan.finch@hp.com>
2023-12-12 14:53:38 +0000

    Add some handy utils

---
+++ utils/maths.py
@@ -0,0 +1,13 @@
+def sum_to_n(n, size, limit=None):
+    """Produce all lists of `size` positive integers in decreasing order
+    that add up to `n`."""
+    if size == 1:
+        yield [n]
+        return
+    if limit is None:
+        limit = n
+    start = (n + size - 1) // size
+    stop = min(limit, n - size + 1) + 1
+    for i in range(start, stop):
+        for tail in sum_to_n(n - i, size - 1, i):
+            yield [i] + tail
